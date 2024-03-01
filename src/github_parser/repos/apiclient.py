b7348b1a3b2b7b0594caadcf9458e11362ecd0bd
Djama1GIT <dj.ama1@mail.ru>
2024-03-01 15:14:46 +0300

    feat: repo activity

--- src/github_parser/repos/apiclient.py
+++ src/github_parser/repos/apiclient.py
@@ -1,3 +1,5 @@
+from datetime import datetime, date
+
 import requests
 from typing import List
 
@@ -7,16 +9,7 @@ from . import schemas
 from ..logger import logger
 
 
-@cache(expire=3600)
-def fetch_repositories(sort_method: str = "stars") -> List[schemas.Repository]:
-    logger.info("fetch")
-    url = f"https://api.github.com/search/repositories?q={sort_method}:%3E1&sort={sort_method}&per_page=100"
-    response = requests.get(url)
-    data = response.json()
-
-    if response.status_code != 200:
-        raise Exception(f"Request failed with status code {response.status_code}")
-
+def serialize_repositories(data: dict) -> List[schemas.Repository]:
     repositories = [
         schemas.Repository(
             repo=repository["full_name"],
@@ -33,3 +26,83 @@ def fetch_repositories(sort_method: str = "stars") -> List[schemas.Repository]:
     ]
 
     return repositories
+
+
+@cache(expire=3600)
+def fetch_top_repositories(sorting_method: str = "stars", per_page: int = 100) -> List[schemas.Repository]:
+    logger.info(f"Fetch top repositories, "
+                f"sorting method: {sorting_method}")
+
+    url = f"https://api.github.com/search/repositories?" \
+          f"q={sorting_method}:>1&" \
+          f"sort={sorting_method}&" \
+          f"per_page={per_page}&"
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+    response = requests.get(url, headers)
+    data = response.json()
+
+    if response.status_code != 200:
+        logger.error(f"An error has occurred while fetching top repositories, "
+                     f"sorting method: {sorting_method}")
+        logger.error(response)
+        raise Exception(f"Request failed with status code {response.status_code}")
+
+    repositories = serialize_repositories(data)
+
+    return repositories
+
+
+def serialize_commits_data_order_by_date(commits_data: dict) -> List[schemas.RepoActivity]:
+    activity = {}
+
+    for commit in commits_data:
+        commit_date = datetime.strptime(
+            commit['commit']['author']['date'],
+            "%Y-%m-%dT%H:%M:%SZ"
+        )
+        commit_date = commit_date.strftime("%Y-%m-%d")
+        author = commit['commit']['author']['name']
+
+        if commit_date not in activity:
+            activity[commit_date] = schemas.RepoActivity(
+                date=commit_date,
+                commits=1,
+                authors=[author],
+            )
+        else:
+            activity[commit_date].commits += 1
+            if author not in activity[commit_date].authors:
+                activity[commit_date].authors.append(author)
+
+    return list(activity.values())
+
+
+@cache(expire=60)
+def fetch_repo_activity(owner: str, repo: str, since: date,
+                        until: date, per_page=9999) -> List[schemas.RepoActivity]:
+    logger.info(f"Fetch repo activity, "
+                f"owner: {owner}, "
+                f"repo: {repo}, "
+                f"since: {since}, "
+                f"until: {until}")
+
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits?" \
+          f"since={since}&" \
+          f"until={until}&" \
+          f"per_page={per_page}"
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        logger.error(f"Error when executing the repo activity, "
+                     f"owner: {owner}, "
+                     f"repo: {repo}, "
+                     f"since: {since}, "
+                     f"until: {until}")
+        logger.error(response)
+        raise Exception(f"Error when executing the request: {response.status_code}")
+
+    commits_data = response.json()
+    activity_list = serialize_commits_data_order_by_date(commits_data)
+
+    return activity_list
--- src/github_parser/repos/router.py
+++ src/github_parser/repos/router.py
@@ -1,16 +1,12 @@
-# import datetime
-# import re
-from datetime import date, datetime
-from typing import List
+from datetime import date
+from typing import List, Optional
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Query, HTTPException
 
 from . import schemas
-from .apiclient import fetch_repositories
+from .apiclient import fetch_top_repositories, fetch_repo_activity
 from ..logger import logger
 
-# from fastapi.responses import JSONResponse, Response
-#
 # from db import get_async_session
 # from utils.logger import logger
 # from .dependencies import get_ref_repository
@@ -26,16 +22,24 @@ router = APIRouter(
 
 
 @router.get('/top100', response_model=List[schemas.Repository])
-async def top100():
-    res = await fetch_repositories()
-    logger
-    return res
+async def top100(
+        sorting_method: Optional[str] = Query(
+            "stars",
+            description="Sorting methods: stars, forks, help-wanted-issues, updated"
+        )
+):
+    logger.info(f"Received request for top 100 repositories with sorting method: {sorting_method}")
+
+    if sorting_method not in ["stars", "forks", "help-wanted-issues", "updated"]:
+        logger.error(f"Invalid sorting method: {sorting_method}")
+        raise HTTPException(status_code=422, detail="Incorrect sorting method")
+
+    top = await fetch_top_repositories(sorting_method)
+    return top
 
 
 @router.get('{owner}/{repo}/activity', response_model=List[schemas.RepoActivity])
-def repo_activity(owner: str, repo: str, since: date, until: date):
-    return [schemas.RepoActivity(
-        date=datetime(2024, 3, 1),
-        commits=27,
-        authors=["GADJIIAVOV", "DJAMAL"]
-    ).__dict__]
+async def repo_activity(owner: str, repo: str, since: date, until: date):
+    logger.info(f"Received request for repository activity for {owner}/{repo} from {since} to {until}")
+    activity = await fetch_repo_activity(owner, repo, since, until)
+    return activity
